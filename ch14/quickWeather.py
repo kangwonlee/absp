0316a8c57bce918636d53c82e3cab35d327a8cbf
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-06-08 21:59:19 +0900

    f-string (may need a personal account)

--- ch14/quickWeather.py
+++ ch14/quickWeather.py
@@ -3,14 +3,16 @@
 
 import json, requests, sys
 
+
 # Compute location from command line arguments.
 if len(sys.argv) < 2:
     print('Usage: quickWeather.py location')
     sys.exit()
+
 location = ' '.join(sys.argv[1:])
 
 # Download the JSON data from OpenWeatherMap.org's API
-url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % (location)
+url =f'http://api.openweathermap.org/data/2.5/forecast/daily?q={location}&cnt=3'
 response = requests.get(url)
 response.raise_for_status()
 
@@ -19,7 +21,7 @@ weatherData = json.loads(response.text)
 
 # Print weather descriptions.
 w = weatherData['list']
-print('Current weather in %s:' % (location))
+print(f'Current weather in {location}:')
 print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
 print()
 print('Tomorrow:')
