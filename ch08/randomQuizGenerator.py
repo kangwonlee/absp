8ea2759402c337ecd21a463efa96dde64b6b5d26
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-06-08 21:49:31 +0900

    f-string

--- ch08/randomQuizGenerator.py
+++ ch08/randomQuizGenerator.py
@@ -11,12 +11,12 @@ capitalsItems = list(capitals.items())
 # Generate 35 quiz files.
 for quizNum in range(35): 
     # Create the quiz and answer key files.
-    quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w')
-    answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+    quizFile = open(f'capitalsquiz{quizNum + 1}.txt', 'w')
+    answerKeyFile = open(f'capitalsquiz_answers{quizNum + 1}.txt', 'w')
 
     # Write out the header for the quiz.
     quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
-    quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+    quizFile.write((' ' * 20) + f'State Capitals Quiz (Form {quizNum + 1})')
     quizFile.write('\n\n')
 
     # Shuffle the order of the states.
@@ -36,12 +36,12 @@ for quizNum in range(35):
         random.shuffle(answerOptions) # randomize the order of the answers
 
         # Write the question and answer options to the quiz file.
-        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+        quizFile.write(f'{questionNum + 1}. What is the capital of {states[questionNum]}?\n')
         for i in range(4):
-            quizFile.write('    %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+            quizFile.write(f"    {'ABCD'[i]}. {answerOptions[i]}\n")
         quizFile.write('\n')
 
         # Write out the answer key to a file.
-        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+        answerKeyFile.write("{questionNum + 1}. {'ABCD'[answerOptions.index(correctAnswer)]}\n")
     quizFile.close()
     answerKeyFile.close()
