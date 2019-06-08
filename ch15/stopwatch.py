8568ec107971b9b8672f71d46dcb38db0f34e95f
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-06-08 22:37:08 +0900

    f-string

--- ch15/stopwatch.py
+++ ch15/stopwatch.py
@@ -17,7 +17,7 @@ try:
         input()
         lapTime = round(time.time() - lastTime, 2)
         totalTime = round(time.time() - startTime, 2)
-        print('Lap #%s: %s (%s)' % (lapNum, totalTime, lapTime), end='')
+        print(f'Lap #{lapNum}: {totalTime} ({lapTime})', end='')
         lapNum += 1
         lastTime = time.time() # reset the last lap time
 except KeyboardInterrupt:
